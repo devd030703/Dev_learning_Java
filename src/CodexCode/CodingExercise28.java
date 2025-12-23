f85e9b311641ab45dd986bf65fbf7303acc75153
devdeepak <devdeepak.deepak@gmail.com>
2025-12-23 09:12:12 +0530

    coding exercise 28

--- src/CodexCode/CodingExercise28.java
+++ src/CodexCode/CodingExercise28.java
@@ -1,5 +1,29 @@
 package CodexCode;
 
+import java.util.Scanner;
+
 public class CodingExercise28 {
 
+    public static void main(String[] args) {
+        inputThenPrintSumAndAverage();
+    }
+
+    public static void inputThenPrintSumAndAverage() {
+        Scanner scanner = new Scanner(System.in);
+        long sum = 0;
+        long count = 0;
+
+        while (true) {
+            if (scanner.hasNextInt()) {
+                int number = scanner.nextInt();
+                sum += number;
+                count++;
+            } else {
+                break;
+            }
+        }
+
+        long average = count > 0 ? Math.round((double) sum / count) : 0;
+        System.out.println("SUM = " + sum + " AVG = " + average);
+    }
 }
