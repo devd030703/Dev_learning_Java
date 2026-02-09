c9df520a925878a14b2e072ca671c8ada4d0418b
devdeepak <devdeepak.deepak@gmail.com>
2026-02-09 18:41:51 +0000

    Inheritance

---
+++ src/Inheritance/Dog.java
@@ -0,0 +1,68 @@
+package Inheritance;
+
+public class Dog extends Animal {
+
+    private String earShape;
+    private String tailShape;
+
+    public Dog () {
+        super("Dog", "Big", 50);
+    }
+
+    public Dog(String type, double weight, String earShape, String tailShape) {
+        super(type, weight < 15 ? "small" : (weight < 35 ? "medium" : "large"), weight);
+        this.earShape = earShape;
+        this.tailShape = tailShape;
+    }
+
+    public Dog (String type, double weight) {
+        this(type, weight, "Perky", "Curled");
+    }
+
+    private void bark() {
+        System.out.println("Woof! ");
+    }
+
+    private void run() {
+        System.out.println("Dog Running ");
+    }
+
+    private void walk() {
+        System.out.println("Dog Walking ");
+    }
+
+    private void wagTail() {
+        System.out.println("Dog Wagging ");
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "earShape='" + earShape + '\'' +
+                ", tailShape='" + tailShape + '\'' +
+                "} " + super.toString();
+    }
+
+    @Override
+    public void makeNoise() {
+        if (type == "Wolf"){
+            System.out.println("Ow Wooo! ");
+        } else {
+            bark();
+        }
+    }
+
+    @Override
+    public void move(String speed) {
+        super.move(speed);
+        System.out.println("Dogs walk, run and wag their tail");
+        if (speed == "slow") {
+            walk();
+            wagTail();
+        } else {
+            run();
+            bark();
+        }
+        System.out.println();
+    }
+}
