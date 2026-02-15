21285b51eb729f87ec9e16fcbfc4a033447591fd
devdeepak <devdeepak.deepak@gmail.com>
2026-02-15 21:37:30 +0000

    Text blocks and advanced output formatting

---
+++ src/TextBlock/Main.java
@@ -0,0 +1,41 @@
+package TextBlock;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String bulletIT = "Print a bullet list: \n" +
+                "\t\u2022 First Point \n" +
+                "\t\t\u2022 Sub Point";
+
+        System.out.println(bulletIT);
+
+        String textBlock = """
+                Print a Bulleted List:
+                    • First Point
+                        • Second Point""";
+
+        System.out.println(textBlock);
+
+        int age = 35;
+        System.out.printf("your age is %d%n", age);
+
+        int yearOfBirth = 2023 - age;
+        System.out.printf("Age = %d, Birth year = %d%n", age, yearOfBirth);
+
+        System.out.printf("Age = %.2f, Birth year = %d%n", (float) age, yearOfBirth);
+
+        for (int i = 1; i < 100000; i *= 10) {
+            System.out.printf("Printing %d %n", i);
+        }
+
+        for (int i = 1; i < 100000; i *= 10) {
+            System.out.printf("Printing %6d %n", i);
+        }
+
+        String formattedString = String.format("Your age is %d", age);
+        System.out.println(formattedString);
+
+        formattedString = "Your age is %d".formatted(age);
+        System.out.println(formattedString);
+    }
+}
