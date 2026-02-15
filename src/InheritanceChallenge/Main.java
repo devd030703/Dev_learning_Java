ce47c27baa4737096bd0a9255abc219b4bac9bcd
devdeepak <devdeepak.deepak@gmail.com>
2026-02-15 20:26:25 +0000

    Inheritance Challenge Part 1

--- src/InheritanceChallenge/Main.java
+++ src/InheritanceChallenge/Main.java
@@ -1,4 +1,19 @@
 package InheritanceChallenge;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Employee tim = new Employee("Tim", "11/11/1985", "01/01/2020");
+
+        System.out.println(tim);
+        System.out.println("Age = " + tim.getAge());
+        System.out.println("Pay = " + tim.collectPay());
+
+        Employee joe = new Employee("Joe", "03/03/1980", "01/01/2025");
+
+        System.out.println(joe);
+        System.out.println("Age = " + joe.getAge());
+        System.out.println("Pay = " + joe.collectPay());
+    }
 }
