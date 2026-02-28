ccf271ef5ead2ae2923393dbef5f6db8b1e9561b
devdeepak <devdeepak.deepak@gmail.com>
2026-02-28 17:55:47 +0000

    CodingExercise37

---
+++ src/CodingExercise37Cuboid.java
@@ -0,0 +1,16 @@
+public class CodingExercise37Cuboid extends CodingExercise37Rectangle {
+    private double height;
+
+    public CodingExercise37Cuboid(double width, double length, double height) {
+        super(width, length);
+        this.height = (height < 0) ? 0 : height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+        return getArea() * height;
+    }
+}
