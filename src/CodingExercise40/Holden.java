6c84e74719d3a746b81bb517f19ba706a2628b22
devdeepak <devdeepak.deepak@gmail.com>
2026-03-06 18:09:31 +0000

    Coding Exercise 40

---
+++ src/CodingExercise40/Holden.java
@@ -0,0 +1,23 @@
+package CodingExercise40;
+
+public class Holden extends Car {
+
+    public Holden(int cylinder, String name) {
+        super(cylinder, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Holden -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Holden -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return "Holden -> brake()";
+    }
+}
