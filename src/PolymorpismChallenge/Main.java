43eb925e126c3736fed2a540a7f454dbab12d335
Dev Deepak <dev.deepak1@starlingbank.com>
2026-03-06 17:35:54 +0000

    Polymorphism Challenge

---
+++ src/PolymorpismChallenge/Main.java
@@ -0,0 +1,16 @@
+package PolymorpismChallenge;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Car car = new Car ("2022 Blue Ferrari 296 GTS");
+        runRace(car);
+
+    }
+
+    public static void runRace(Car car) {
+
+        car.startEngine();;
+        car.drive();
+    }
+}
