27a11dd357ffa818e08b237f85d5cacb1a618bdb
devdeepak <devdeepak.deepak@gmail.com>
2026-04-13 22:42:22 +0100

    Arrays challenge

---
+++ src/CodexCode/CodingExercise41.java
@@ -0,0 +1,48 @@
+package CodexCode;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CodingExercise41 {
+
+    public static int[] getIntegers(int size) {
+
+        Scanner scanner = new Scanner(System.in);
+        int[] values = new int[size];
+
+        for (int i = 0; i < values.length; i++) {
+            values[i] = scanner.nextInt();
+        }
+
+        return values;
+    }
+
+    public static void printArray(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Element " + i + " contents " + array[i]);
+        }
+    }
+
+    public static int[] sortIntegers(int[] array) {
+
+        int[] sortedArray = Arrays.copyOf(array, array.length);
+        boolean flag = true;
+        int temp;
+
+        while (flag) {
+            flag = false;
+
+            for (int i = 0; i < sortedArray.length - 1; i++) {
+                if (sortedArray[i] < sortedArray[i + 1]) {
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i + 1];
+                    sortedArray[i + 1] = temp;
+                    flag = true;
+                }
+            }
+        }
+        return sortedArray;
+    }
+
+}
